c977d0a42e072809a065dd959f10565c234966cf
vuongquocanh02th <vuongquocanh300402@gmail.com>
2024-11-04 21:32:06 +0700

    Khai bao bien

---
+++ VariableJava/VarDataType/src/VarDataType.java
@@ -0,0 +1,16 @@
+public class VarDataType {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'c';
+        String str = "abc";
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("str = " + str);
+
+    }
+}
